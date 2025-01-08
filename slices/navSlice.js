4558b4d408d619d8401feb9f4411dc83b842c488
Pedro Firmino <pedrohefirmino@gmail.com>
2025-01-08 15:48:32 -0300

    navslice

---
+++ slices/navSlice.js
@@ -0,0 +1,37 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+    origin: null,
+    destination: null,
+    travelTimeInformation: null
+
+}
+
+export const navSlice = createSlice ({
+    name: 'nav',
+    initialState,
+    reducer: {
+        setOrigin: (state, action) => {
+            state.origin = action.payload;
+        },
+        setDestination: (state, action) => {
+            state.destination = action.payload;
+        },
+        setTravelTimeInformation: (state, action) => {
+            state.TravelTimeInformatio = action.payload;
+        },
+    },
+});
+
+export const { setDestination, setOrigin, setTravelTimeInformation } = navSlice.actions;
+
+
+//Selectors
+
+export const selectOrigin = (state) => state.nav.origin;
+
+export const selectDestination = (state) => state.nav.destination;
+
+export const selectTravelTimeInformation = (state) => state.nav.TravelTimeInformation;
+
+export default navSlice.reducer;
