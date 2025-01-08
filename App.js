76b8161335fb01cd074da0d9e88b5ec926776ff0
Pedro Firmino <pedrohefirmino@gmail.com>
2025-01-08 15:22:28 -0300

    Start

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from "react-redux";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Flippy Express</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
