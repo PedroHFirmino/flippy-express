273b1dc908342baf463483728c4e28891b7587c2
unknown <pedrohefirmino@gmail.com>
2025-06-02 21:49:11 -0300

    Navigate Card

--- user_components/NavigateCard.js
+++ user_components/NavigateCard.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import tw from 'twrnc';
+import LocationAutocomplete from './LocationAutocomplete';
+import {GOOGLE_MAPS_API_KEY} from '@env';
 
 const NavigateCard = () => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Hello, React Native!</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.text}>Olá Pedro</Text>
+      <View style={tw `border-t border-gray-200 flex-shrink`}>
+      <View>
+      <LocationAutocomplete 
+        placeholder="Para onde?"
+        nearbyPlacesAPI='GooglePlacesSearch'
+        debounce={400}
+        query={{
+          key: GOOGLE_MAPS_API_KEY,
+          language: 'pt',
+        }}
+        />
+      </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
 export default NavigateCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  text:{
+    textAlign:'center',
+    paddingTop: 5,
+  },
+
+});
+
--- user_screens/MapScreen.js
+++ user_screens/MapScreen.js
@@ -2,61 +2,45 @@ import React from 'react'
 import tw from 'twrnc';
 import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
 import MapUser from '../user_components/MapUser';
-import MapView from 'react-native-maps';
+import NavigateCard from '../user_components/NavigateCard';
+import { createStackNavigator } from '@react-navigation/stack';
+import buscaOpcoesCard from '../user_components/buscaOpcoesCard';
 
 
 
 const MapScreen = () => {
-    return (
-            <View style={{ flex: 1 }}>
-                    <View style={{ flex: 1 }}>
+    const Stack = createStackNavigator();
 
+    return (
+        <View>
+            <View style={tw`h-1/2`}>
                 <MapUser />
-                
-                    </View>
-            </View>
+        </View>
+        <View style={tw`h-1/2`}>
+            <Stack.Navigator>
+             <Stack.Screen
+                name="NavigateCard"
+                component={NavigateCard}
+                options={{
+                    headerShown: false,
+                }} 
+                />
+             <Stack.Screen
+                name="buscaOpcoesCard"
+                component={buscaOpcoesCard}
+                options={{
+                    headerShown: false,
+                }} 
+                />                 
+                 
+            </Stack.Navigator>
+        </View>
+        </View>
+
 
     );
 };
 
-export default MapScreen
+export default MapScreen;
 
 const styles = StyleSheet.create ({});
-
-
-// import React from 'react'
-// import tw from 'twrnc';
-// import { StyleSheet, Text, View, SafeAreaView, Image } from 'react-native'
-// import MapUser from '../user_components/MapUser';
-// import MapView from 'react-native-maps';
-
-
-
-// const MapScreen = () => {
-//     const Stack = createStackNavigator();
-
-//     return (
-//         <View>
-//             <View style={tw`h-1/2`}>
-//                 <Map />
-//         </View>
-//         <View style={tw`h-1/2`}>
-//             <Stack.Navigator>
-//              <Stack.Screen
-//                 name="NavigateCard"
-//                 component={NavigateCard}
-//                 options={{
-//                     headerShown: false,
-//                 }} 
-//                 />  
-//             </Stack.Navigator>
-//         </View>
-//         </View>
-
-
-//     );
-// };
-
-// export default MapScreen
-
-// const styles = StyleSheet.create ({});
